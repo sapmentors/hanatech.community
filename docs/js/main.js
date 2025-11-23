a75df293d6fd333f9e9f20e60781708ea4414928
Johannes Quast <johannes.quast@sap.com>
2025-11-23 18:59:04 +0100

    Replace date with placeholder

--- docs/js/main.js
+++ docs/js/main.js
@@ -170,7 +170,7 @@ function buildJury() {
  * CONFERENCE CLOCK
  */
 //--------------------------------------------------------------------------------------------------
-const CONFERENCE_TIME = Date.parse('2025-07-10T00:10:00.000+01:00');
+const CONFERENCE_TIME = Date.parse('2026-01-01T00:10:00.000+01:00');
 //--------------------------------------------------------------------------------------------------
 function ensureTwoDigits(time) {
     return time.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
